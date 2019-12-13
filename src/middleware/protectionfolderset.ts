67780d348551933543473de0fe65fc11cc013f95
liqiang <liqiang@163.com>
2019-12-13 13:32:04 +0800

    获取用户名

--- src/middleware/protectionfolderset.ts
+++ src/middleware/protectionfolderset.ts
@@ -8,20 +8,20 @@ export const ref = require('ref');
 export const ffi = require('ffi');
 export const wmi = require('node-wmi');
 
-let libm =
-ffi.Library(join(process.cwd(), './dist/minispy'), {
-    'setProtectionFolder': ['pointer', ['string']],
-    'getProtectionFolder': ['string', ['void']],
-    'setOpenProcess':      ['void', ['string']],
-    'GetRecords':		   ['int',  ['void']],
-    'SetGetRecCb': ['int', [
-    ffi.Function(ref.types.void,[
-    ref.types.CString, 
-    ref.types.char, 
-    ref.types.CString, 
-    ref.types.CString,
-    ref.types.CString])]]
-});
+// let libm =
+// ffi.Library(join(process.cwd(), './dist/minispy'), {
+//     'setProtectionFolder': ['pointer', ['string']],
+//     'getProtectionFolder': ['string', ['void']],
+//     'setOpenProcess':      ['void', ['string']],
+//     'GetRecords':		   ['int',  ['void']],
+//     'SetGetRecCb': ['int', [
+//     ffi.Function(ref.types.void,[
+//     ref.types.CString, 
+//     ref.types.char, 
+//     ref.types.CString, 
+//     ref.types.CString,
+//     ref.types.CString])]]
+// });
 
 let bb = Integer(1);
 bb.plus(11);
@@ -36,32 +36,28 @@ console.log(abuf.toString('ascii'));
 
 
 var bios: any;
-wmi.Query({class:'Win32_UserAccount'},(err, bios) => {
+wmi.Query({class:'Win32_UserAccount'},(err, data) => {
+    bios = data;
     console.log(bios);
 });
 
-
-async function  getUserBySid(sid){
+function  wmiQuery(){
     return new Promise(function(resovle,reject){
         try {
-            bios.forEach(element => {
-                if(element.SID == sid){
-                    return resovle(element.Name);
-                }         
+            wmi.Query({class:'Win32_UserAccount'},(err, data) => {
+                return resovle(bios = data);
             });
         } catch(err) {
-            return resovle("");
+            return resovle(err);
         }
     });
 }
-
-
 export class ProtectionFolderSet{
     constructor(){}
 
     getProtectionFolder(){
          let pFolder = null;
-         pFolder = libm.getProtectionFolder(null);
+        //  pFolder = libm.getProtectionFolder(null);
          console.log(pFolder);
          let buf = ref.allocCString(pFolder);
          console.log(buf.toString());
@@ -75,7 +71,7 @@ export class ProtectionFolderSet{
         abuf = Buffer.concat([abuf,unicode_null],len+len1);
         cstr1 = abuf.toString('ascii');
         console.log(cstr1);
-        libm.setProtectionFolder(cstr1);
+        // libm.setProtectionFolder(cstr1);
     }
 
     setOpenProcess(cstr1){
@@ -86,15 +82,15 @@ export class ProtectionFolderSet{
         abuf = Buffer.concat([abuf,unicode_null],len+len1);
         cstr1 = abuf.toString('ascii');
         console.log(cstr1);
-        libm.setOpenProcess(cstr1);
+        // libm.setOpenProcess(cstr1);
     }
 
     checklogs()
     {
         console.log('check the minisy log now!');
         setTimeout(()=>{
-            libm.SetGetRecCb(this.callback);
-            libm.GetRecords(null);
+            // libm.SetGetRecCb(this.callback);
+            // libm.GetRecords(null);
             this.checklogs();
         }, 1000);
     };
@@ -103,7 +99,6 @@ export class ProtectionFolderSet{
         'void', ['string', 'char', 'string', 'string', 'string'], 
         async function(fName, tag, time, writer,sid) {
             let tag1 = String.fromCharCode(tag); 
-            console.log("fileName: %s\n", fName);
             if(tag1 == "W") {
                 tag1 = "修改";
             }else if(tag1 = "D"){
@@ -111,11 +106,34 @@ export class ProtectionFolderSet{
             }else if(tag1 == "R"){
                 tag1 = "重命名";
             }
-            let userName = await getUserBySid(sid);
+            console.log("fileName: %s\n", fName);
             console.log("tag: %s\n", tag1);
             console.log("time: %s", time);
             console.log("author: %s", writer);
-            save({FileName: fName, AccessType: tag1, AccessTime: time, Author: writer, UserName: userName});
+            let userName;
+            if (bios) {
+                bios.forEach(element => {
+                    if (element.SID == sid) {
+                        userName = element.Name;
+                        console.log("userName: %s\n",userName);
+                        save({FileName: fName, AccessType: tag1, AccessTime: time, Author: writer, UserName: userName});
+                        // return;
+                    }
+                });
+            }
+            else {
+                wmiQuery().then(res => {
+                    bios = res;
+                    bios.forEach(element => {
+                        if (element.SID == sid) {
+                            userName = element.Name;
+                            console.log("userName: %s\n",userName);
+                            save({FileName: fName, AccessType: tag1, AccessTime: time, Author: writer, UserName: userName});
+                            // return;
+                        }
+                    });
+                });
+            }
         }
     )
 }
