761c4685a0b6e3611449a512b719a88a6e09f2ba
victor.thrifts <jiamswang@163.com>
2019-12-25 17:42:18 +0800

    ssr

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -6,15 +6,23 @@ import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 
+import { en_US, NZ_I18N, NzI18nModule } from 'ng-zorro-antd/i18n';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
     ServerTransferStateModule,
-    ModuleMapLoaderModule
+    ModuleMapLoaderModule,
+    NzI18nModule,
+    NoopAnimationsModule,
+ //   HttpClientModule
   ],
   providers: [
     // Add universal-only providers here
+    { provide: NZ_I18N, useValue: en_US }
   ],
   bootstrap: [ AppComponent ],
 })
