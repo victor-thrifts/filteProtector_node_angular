ecde85f66064de97bf5d4eab0a059b9d71bcbbe3
hai <397343539@qq.com>
2019-12-31 11:17:13 +0800

    调整pdf样式

--- src/app/accloges/accloges.component.ts
+++ src/app/accloges/accloges.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Acclog } from '../_models/acclog';
-import { AcclogService } from '../_services';
+import {Component, OnInit} from '@angular/core';
+import {Acclog} from '../_models/acclog';
+import {AcclogService} from '../_services';
 import * as pdfMake from 'pdfmake/build/pdfmake';
 import * as pdfFonts from 'pdfmake/build/vfs_fonts';
-import { en_US, zh_CN, NzI18nService } from 'ng-zorro-antd/i18n';
-import { NzModalService } from 'ng-zorro-antd/modal';
+import {en_US, zh_CN, NzI18nService} from 'ng-zorro-antd/i18n';
+import {NzModalService} from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-accloges',
@@ -16,13 +16,13 @@ export class AcclogesComponent implements OnInit {
   page = 1; // 当前页
   pageSize = 20; // 每页条数
   pages = []; // 显示页数集合
-  count:number; // 总条数
+  count: number; // 总条数
   pageCount = 0; //总页数
   pageLenght = 6; // 显示页数数量
   pageSizeOptions: number[] = [5, 10, 25, 100];
   pdf: Acclog[];
-  dateRange:'';
-  acclogeForm = {FileName:'',AccessType:'',UserName:'', dateArray:''};
+  dateRange: '';
+  acclogeForm = {FileName: '', AccessType: '', UserName: '', dateArray: ''};
 
   constructor(private acclogService: AcclogService,
               private i18n: NzI18nService,
@@ -39,22 +39,24 @@ export class AcclogesComponent implements OnInit {
 
   getAccloges(): void {
     this.acclogService.getAccloges(this.page, this.pageSize, this.acclogeForm)
-    .subscribe(accloges => this.accloges = accloges);
+      .subscribe(accloges => this.accloges = accloges);
   }
 
   getAcclogCount(): void {
     let vm = this;
     this.acclogService.getAcclogCountByQuery(this.acclogeForm)
-    .subscribe(pageInfo => {
-      vm.count = pageInfo.count;
-      vm.getAccloges();
-      vm.calculateIndexes();
-    });
+      .subscribe(pageInfo => {
+        vm.count = pageInfo.count;
+        vm.getAccloges();
+        vm.calculateIndexes();
+      });
   }
 
   add(name: string): void {
     name = name.trim();
-    if (!name) { return; }
+    if (!name) {
+      return;
+    }
     this.acclogService.addAcclog(name)
       .subscribe(acclog => {
         this.accloges.push(acclog);
@@ -63,12 +65,12 @@ export class AcclogesComponent implements OnInit {
 
   delete(acclog: Acclog): void {
     this.acclogService.deleteAcclog(acclog)
-        .subscribe(() => {
-          this.accloges = this.accloges.filter(h => h !== acclog);
-        });
+      .subscribe(() => {
+        this.accloges = this.accloges.filter(h => h !== acclog);
+      });
   }
 
-  queryFileLog(){
+  queryFileLog() {
     this.page = 1;
     this.getAcclogCount();
   }
@@ -76,13 +78,15 @@ export class AcclogesComponent implements OnInit {
   showConfirm(): void {
     this.modalService.confirm({
       nzTitle: '<i>请确认是否导出日志?</i>',
-      nzOnOk: () => {this.exportLog()}
+      nzOnOk: () => {
+        this.exportLog()
+      }
     });
   }
 
   exportLog() {
     // console.log(this.count);
-    this.acclogService.getAccloges(1, this.count,this.acclogeForm).subscribe(
+    this.acclogService.getAccloges(1, this.count, this.acclogeForm).subscribe(
       pdf => {
         this.pdf = pdf;
         if (this.count === this.pdf.length) {
@@ -99,6 +103,7 @@ export class AcclogesComponent implements OnInit {
             arr[5] = this.pdf[i].Author;
             arrayData.push(arr);
           }
+
           this.pdfmakes(arrayData);
         }
       }
@@ -106,6 +111,7 @@ export class AcclogesComponent implements OnInit {
   }
 
   pdfmakes(arrayData) {
+    let user = JSON.parse(sessionStorage.getItem('currentUser')).user;
     pdfMake.vfs = pdfFonts.pdfMake.vfs;
     pdfMake.fonts = {
       Roboto: {
@@ -123,84 +129,95 @@ export class AcclogesComponent implements OnInit {
       }
     }
     let data = {
-      content: [{
+      content: [
+        {text: '文件日志列表', fontSize: 20, bold: true, alignment: 'center'},
+        {text: '导出人:'+ user, alignment: 'right', lineHeight:'1.5'},
+        {text: '导出时间:'+new Date().toLocaleDateString(),alignment: 'right', lineHeight:'1.5'},
+        {
         layout: 'lightHorizontalLines', // optional
         table: {
           headerRows: 1,
-          widths: [ 40, 100, 60, 65 ,65,100],
-          body: arrayData
-        }
+          // widths: [40, 100, 60, 65, 65, 100],
+          widths: [60, 140, 80, 80, 150, 140],
+          body: arrayData,
+        },
+
       }],
       defaultStyle: {
         font: 'jdstj'
       },
+      footer: function(currentPage, pageCount) {
+        let page = currentPage.toString() + '/' + pageCount;
+         return {text: page, alignment: 'center'}
+        },
+      pageOrientation: 'landscape',
     };
     pdfMake.createPdf(data).download();
   }
 
-  next(dis:number): void {
-    if(dis == this.pageCount){
+  next(dis: number): void {
+    if (dis == this.pageCount) {
       return;
     }
     this.page = this.page + 1;
-    this.acclogService.getAccloges(this.page, this.pageSize,this.acclogeForm)
-    .subscribe(accloges => this.accloges = accloges);
-    this.calculateIndexes ();
+    this.acclogService.getAccloges(this.page, this.pageSize, this.acclogeForm)
+      .subscribe(accloges => this.accloges = accloges);
+    this.calculateIndexes();
   }
 
-  prev(dis:number): void {
-    if(dis == 1){
+  prev(dis: number): void {
+    if (dis == 1) {
       return;
     }
     this.page = this.page - 1;
-    this.acclogService.getAccloges(this.page, this.pageSize,this.acclogeForm)
-    .subscribe(accloges => this.accloges = accloges);
-    this.calculateIndexes ();
+    this.acclogService.getAccloges(this.page, this.pageSize, this.acclogeForm)
+      .subscribe(accloges => this.accloges = accloges);
+    this.calculateIndexes();
   }
 
-  selectPage(page:number): void {
+  selectPage(page: number): void {
     this.page = page;
-    this.acclogService.getAccloges(this.page, this.pageSize,this.acclogeForm)
-    .subscribe(accloges => this.accloges = accloges);
-    this.calculateIndexes ();
+    this.acclogService.getAccloges(this.page, this.pageSize, this.acclogeForm)
+      .subscribe(accloges => this.accloges = accloges);
+    this.calculateIndexes();
   }
 
   // 分页算法
-  calculateIndexes (): void {
+  calculateIndexes(): void {
     // this.getAcclogCount();
     console.log(this.count);
     this.pages = [];
-    this.pageCount = Math.ceil(this.count/this.pageSize);
+    this.pageCount = Math.ceil(this.count / this.pageSize);
     // 普通情况，页数中没有首页和尾页
     var start = Math.round(this.page - this.pageLenght / 2);
-    var end = Math.round(this.page + this.pageLenght / 2)-1;
+    var end = Math.round(this.page + this.pageLenght / 2) - 1;
     //页数中有首页
-    if (start <1) {
+    if (start < 1) {
       // console.log(start+"小于1")
       start = 1;
       // 默认显示的最后一个数字为设置的页码显示长度
       end = this.pageLenght;
       if (end >= this.pageCount) {
-          // console.log(end+"Da于"+length)
-          // 短于设置的页码数，则为其本身长度
-          end = this.pageCount;
-      }
-    }else if(end >= this.pageCount){
-        //页数中有尾页
-        //     console.log(end+"大于等于"+length)
+        // console.log(end+"Da于"+length)
+        // 短于设置的页码数，则为其本身长度
         end = this.pageCount;
-        start = end - this.pageLenght + 1;
-        if (start <= 1) {
-            start = 1;
-        }
+      }
+    } else if (end >= this.pageCount) {
+      //页数中有尾页
+      //     console.log(end+"大于等于"+length)
+      end = this.pageCount;
+      start = end - this.pageLenght + 1;
+      if (start <= 1) {
+        start = 1;
+      }
     }
     // 遍历生成数组
     for (var i = start; i <= end; i++) {
-        this.pages.push(i);
+      this.pages.push(i);
     }
   }
 
-    onChange(result: Date): void {
-      this.acclogeForm.dateArray = JSON.stringify(result);
-    }
+  onChange(result: Date): void {
+    this.acclogeForm.dateArray = JSON.stringify(result);
+  }
 }
